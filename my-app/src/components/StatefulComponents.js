1b97b90c55f93c2c12c3ba2253abcf78d7590147
Mihaela Younas <mihaelayounas55@gmail.com>
2022-11-02 12:09:30 +0000

    Work with state components

--- my-app/src/components/StatefulComponents.js
+++ my-app/src/components/StatefulComponents.js
@@ -1,8 +1,31 @@
 import React from "react";
 
 class StatefulGreeting extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            introduction : "Hello!",
+            buttonText :"Exit",
+        };
+    }
+    handleClick(){
+        this.setState ({
+            introduction:"Goodbye!",
+            buttonText: "Goodbye button"
+        }
+        )
+        console.log(this.state.introduction)
+        console.log(this.state.buttonText)
+    }
+
+
     render(){
-        return <h1>Hi {this.props.greeting}</h1>
+        return(
+            <div>
+                <h1>{this.state.introduction} {this.props.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
+        )
     }
 }
 
