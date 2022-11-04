99a63cb0b26c13336b09bb2aa484fd30ce3b88f7
Mihaela Younas <mihaelayounas55@gmail.com>
2022-11-04 12:00:55 +0000

    Work with event handler click event

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from './components/StatefulComponents';
-import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
-import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
-import NavBarSimple from './components/NavBarSimple';
-import SideBar from './components/SideBar';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from "./components/EventsClass";
+// import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+// import StatefulGreeting from './components/StatefulComponents';
+// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+// import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+// import NavBarSimple from './components/NavBarSimple';
+// import SideBar from './components/SideBar';
 
 function App() {
-  return (
-    <div className="App">
-      <NavBarSimple  />
-     <SideBar />
-    </div>
-  );
+  return <div className="App">
+      <EventsFunctional />
+      <EventsClass />
+  </div>;
 }
 
 export default App;
