0241e5cd3ac082a5484636dac70312ea6e381781
Mihaela Younas <mihaelayounas55@gmail.com>
2022-11-14 13:30:40 +0000

    finalising react challenges with api's

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,21 +9,23 @@ import RenderingLists from './components/RenderingLists';
 // import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
 import SideBar from './components/SideBar';
-import Content from './components/Content';
 import ControlledForm from './components/ControlledForm';
-import SearchBar from './components/SearchBar'
-import ContentHooks from './components/ContentHooks';
-import HTTPPost from './components/HTTPPost';
-
+import SearchBar from './components/SearchBar';
+import PostItemAPI from './components/PostItemAPI';
+import ContentAPI from './components/ContentAPI';
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 function App() {
   return (
     <div className={css.App}>
-    <HTTPPost />
-   
-  
+      {/* Add your components here */}
+      <NavBarForm />
+      <SideBar />
+      {/* <Content /> */}
+      {/* <ContentHooks /> */}
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
     </div>
   );
 }
-
 export default App;
